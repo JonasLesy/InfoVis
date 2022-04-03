3e1d75185d2aaf6e68fd8a3ebfa7633415a82926
Jonas Lesy <jonas.lesy@formica.digital>
2022-04-03 18:25:45 +0200

    Add dataset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PieChartComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
