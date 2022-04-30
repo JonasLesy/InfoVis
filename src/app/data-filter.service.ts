e5f6121b127049d62ffb4259d97206d06ddad673
JorisCraessaerts <33348068+JorisCraessaerts@users.noreply.github.com>
2022-04-30 20:28:50 +0200

    Filter Service continue

--- src/app/data-filter.service.ts
+++ src/app/data-filter.service.ts
@@ -7,25 +7,23 @@ import { CsvData } from './models/csv-data';
   providedIn: 'root'
 })
 export class DataFilterService {
-  private csvData: CsvData;
-
-  private _filteredAthleteEntriesList: AthleteEntry[];
-  private _countrySuggestions: string[];
-  private _peopleSuggestions: string[];
+  private _csvData: CsvData;
   private _countriesToFilterOn: string[] = [];
   private _peopleToFilterOn: string[] = [];
 
 
 
-
+  private _filteredAthleteEntriesList: AthleteEntry[];
   public get filteredAthleteEntriesList(): AthleteEntry[] {
     return this._filteredAthleteEntriesList;
   }
 
+  private _countrySuggestions: string[];
   public get countrySuggestions(): string[] {
     return this._countrySuggestions;
   }
 
+  private _peopleSuggestions: string[];
   public get peopleSuggestions(): string[] {
     return this._peopleSuggestions;
   }
@@ -33,37 +31,37 @@ export class DataFilterService {
   constructor(private csvService: CsvService) {
     csvService.loadCsvData().subscribe(
       (csvData) => {
-        this.csvData = csvData; this._filteredAthleteEntriesList = this.csvData.athleteEntries
+        this._csvData = csvData; this._filteredAthleteEntriesList = this._csvData.athleteEntries
       });
   }
 
   //Gaat nog niet werken, want data in csvService kan misschien nog niet ingeladen zijn op moment dat gefilterd wordt.
   search(searchText: string): void {
     if (searchText !== "") {
-      this._filteredAthleteEntriesList = this.csvService.athleteEntries.filter(contact => {
+      this._filteredAthleteEntriesList = this._csvData.athleteEntries.filter(contact => {
         return contact.name.toLowerCase().includes(searchText.toLowerCase());
       });
     } else {
-      this._filteredAthleteEntriesList = this.csvService.athleteEntries;
+      this._filteredAthleteEntriesList = this._csvData.athleteEntries;
     }
   }
 
   searchCountry(searchText: string): void {
-    let nocsFiltered = this.csvService.nocEntries.filter(nocEntry => {
+    let nocsFiltered = this._csvData.nocRegionEntries.filter(nocEntry => {
       return nocEntry.region.toLowerCase().includes(searchText.toLowerCase());
     });
     this._countrySuggestions = nocsFiltered.map(nocEntry => nocEntry.region);
   }
 
   searchPerson(searchText: string): void {
-    this._peopleSuggestions = this.csvService.persons.filter(person => { return person.toLowerCase().includes(searchText.toLowerCase()) });
+    this._peopleSuggestions = this._csvData.persons.filter(person => { return person.toLowerCase().includes(searchText.toLowerCase()) });
   }
 
   filterOnAllAttributes() {
     console.log('Filtering on attributes..');
-    this._filteredAthleteEntriesList = this.csvService.athleteEntries.filter(athleteEntry => {
+    this._filteredAthleteEntriesList = this._csvData.athleteEntries.filter(athleteEntry => {
       // Only add the entry if the selected countries match the athlete
-      if (this._countriesToFilterOn.length != 0 && !this.athleteBelongsToListOfCountries(athleteEntry, this.countriesToFilterOn)) {
+      if (this._countriesToFilterOn.length != 0 && !this.athleteBelongsToListOfCountries(athleteEntry, this._countriesToFilterOn)) {
         return false;
       }
       if (this._peopleToFilterOn.length != 0 && !this._peopleToFilterOn.includes(athleteEntry.name)) {
@@ -79,7 +77,7 @@ export class DataFilterService {
   }
 
   private getRegionForNoc(nocToLookFor) {
-    let nocEntry = this.csvService.nocEntries.find(item => item.noc === nocToLookFor);
+    let nocEntry = this._csvData.nocRegionEntries.find(item => item.noc === nocToLookFor);
     return nocEntry !== undefined ? nocEntry.region : "";
   }
 }
