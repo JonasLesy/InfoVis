908764434c74981e7b4007fab02a2180c0452c79
Jonas Lesy <jonas.lesy@formica.digital>
2022-05-14 10:17:44 +0200

    Add download to pdf button for athleteEntries

--- src/app/person-detail/person-detail.component.ts
+++ src/app/person-detail/person-detail.component.ts
@@ -2,6 +2,9 @@ import { FilteredDataService } from './../filtered-data.service';
 import { Component, OnInit } from '@angular/core';
 import { Athlete } from 'src/models/athlete';
 import { AthleteEntry } from 'src/models/athlete-entry';
+import jsPDF from 'jspdf'
+import autoTable from 'jspdf-autotable'
+import { FilterService } from '../filter.service';
 
 @Component({
   selector: 'app-person-detail',
@@ -10,12 +13,16 @@ import { AthleteEntry } from 'src/models/athlete-entry';
 })
 export class PersonDetailComponent implements OnInit {
 
-  constructor(public filteredDataService: FilteredDataService) { }
+  constructor(public filteredDataService: FilteredDataService, public filterService: FilterService) { }
 
   private _subscription;
   private _subscriptionEntries;
   public selectedAthlete: Athlete;
   public athleteEntries: AthleteEntry[];
+
+  cols: any[];
+
+  exportColumns: any[];
 
   ngOnDestroy(): void {
     if (this._subscription) {
@@ -38,6 +45,12 @@ export class PersonDetailComponent implements OnInit {
         // this.refreshTable();
       }
     );
+
+    this.cols = [
+      ['Year', 'City', 'Sport', 'Event', 'Medal']
+    ];
+
+    this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
   }
 
   calculateEntryTotal(year) {
@@ -53,5 +66,20 @@ export class PersonDetailComponent implements OnInit {
 
     return total;
   }
+
+  exportPdf() {
+    let filename = this.selectedAthlete.name + "_" + this.filterService.yearRange[0] + "-" + this.filterService.yearRange[1];
+    let exportData = this.athleteEntries.map(athleteEntry => {return [athleteEntry.year, athleteEntry.city, athleteEntry.sport, athleteEntry.event, athleteEntry.medal]});
+    const doc = new jsPDF()
+    autoTable(doc, {
+      head: this.cols,
+      body: exportData,
+      didDrawCell: (data) => {
+        console.log(data.column.index)
+      },
+    })
+
+    doc.save(filename)
+  }  
 
 }
